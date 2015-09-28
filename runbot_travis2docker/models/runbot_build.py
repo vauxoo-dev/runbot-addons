c27c61d9690ef4d2265864d8418a068e15c77cfb
Runbot Vauxoo <runbot@vauxoo.com>
2015-09-28 13:40:18 +0000

    squash

--- runbot_travis2docker/models/runbot_build.py
+++ runbot_travis2docker/models/runbot_build.py
@@ -32,11 +32,10 @@ class RunbotBuild(models.Model):
     _inherit = 'runbot.build'
 
     def job_00_init(self, cr, uid, build, lock_path, log_path):
-        if build.branch_id.repo_id.is_travis2docker_build:
-            _logger.info('skipping job_00_init')
-            return MAGIC_PID_RUN_NEXT_JOB
-        return super(runbot_build, self).job_00_init(
-            cr, uid, build, lock_path, log_path)
+        if not build.branch_id.repo_id.is_travis2docker_build:
+            return super(runbot_build, self).job_00_init(
+                cr, uid, build, lock_path, log_path)
+        raise NotImplemented("ToDo: Build docker image")
 
     def job_10_test_base(self, cr, uid, build, lock_path, log_path):
         if build.branch_id.repo_id.is_travis2docker_build:
@@ -45,13 +44,23 @@ class RunbotBuild(models.Model):
         return super(runbot_build, self).job_10_test_base(
             cr, uid, build, lock_path, log_path)
 
+    def job_20_test_all(self, cr, uid, build, lock_path, log_path):
+        if not build.branch_id.repo_id.is_travis2docker_build:
+            return super(runbot_build, self).job_20_test_all(
+                cr, uid, build, lock_path, log_path)
+        raise NotImplemented("ToDo: Run travis container expose "
+                             "port 8069 to build port")
+
     @custom_build
     def checkout(self, cr, uid, ids, context=None):
         """Save travis2docker output"""
         # runbot log path
-        import pdb;pdb.set_trace()
         for build in self.browse(cr, uid, ids, context=context):
-            run(['travisfile2dockerfile', build.repo_id.name, build.name])
-    #TODO: Add custom build to drop and kill
+            branch_short_name = build.branch_id.replace(
+                'refs/heads/', 1).replace('refs/pull/', 1)
+            t2d_path = os.path.join(os.path.dirname(repo.path), 'travis2docker')
+            run(['travisfile2dockerfile', build.repo_id.name,
+                 branch_short_name, '--root-path=' + t2d_path])
 
+    #TODO: Add custom_build to drop and kill
 
