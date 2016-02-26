e8aa72127c92880ad0580d834c4c395181815069
Moisés López <moylop260@vauxoo.com>
2016-02-26 05:44:21 +0000

    squash

--- runbot_travis2docker/models/runbot_build.py
+++ runbot_travis2docker/models/runbot_build.py
@@ -121,6 +121,7 @@ class RunbotBuild(models.Model):
             '-e', 'RUNBOT=1',
             '-e', 'UNBUFFER=1',
             '-e', 'START_SSH=1',
+            '-e', 'CACHE=1',
             '-p', '%d:%d' % (build.port, 8069),
             '-p', '%d:%d' % (build.port + 1, 22),
         ] + pr_cmd_env + [
