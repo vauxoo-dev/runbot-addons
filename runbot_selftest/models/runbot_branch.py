45537f976bdf2c7507d92a0feaedba241e9240f2
Moisés López <moylop260@vauxoo.com>
2016-03-03 06:56:06 +0000

    [DEL] runbot_selftest: Removing branch fields

--- runbot_selftest/models/runbot_branch.py
+++ runbot_selftest/models/runbot_branch.py
@@ -6,32 +6,32 @@
 from openerp import api, fields, models
 
 
-class RunbotBranch(models.Model):
-    _inherit = "runbot.branch"
+# class RunbotBranch(models.Model):
+#     _inherit = "runbot.branch"
 
-    pr_ok = fields.Boolean('Is pull request?',
-                           compute='_compute_git_data', store=True)
-    branch_base_id = fields.Many2one('runbot.branch', string='Branch Base',
-                                     compute='_compute_git_data', store=True)
+#     # pr_ok = fields.Boolean('Is pull request?',
+#     #                        compute='_compute_git_data', store=True)
+#     # branch_base_id = fields.Many2one('runbot.branch', string='Branch Base',
+#     #                                  compute='_compute_git_data', store=True)
 
-    @api.depends('name')
-    def _compute_git_data(self):
-        # The main method to get closest branch is in repo.build model :(
-        build = self.env['runbot.build']
-        for branch in self:
-            # Method `create` of runbot.build is inherited returning None :(
-            self.env.cr.execute('SAVEPOINT "build_dummy"')
-            build.create(
-                {'branch_id': branch.id, 'name': 'dummy'})
-            build_dummy = build.search([
-                ('branch_id', '=', branch.id), ('name', '=', 'dummy')],
-                limit=1, order='id desc')
-            repo_id, closest_name, server_match = \
-                build_dummy._get_closest_branch_name(branch.repo_id.id)
-            self.env.cr.execute('ROLLBACK TO SAVEPOINT "build_dummy"')
-            branch_base = self.search([
-                ('repo_id', '=', repo_id), ('name', '=', closest_name)],
-                limit=1)
-            if branch_base.id and branch_base.id != branch.id:
-                branch.branch_base_id = branch_base.id
-                branch.pr_ok = True
+#     @api.depends('name')
+#     def _compute_git_data(self):
+#         # The main method to get closest branch is in repo.build model :(
+#         build = self.env['runbot.build']
+#         for branch in self:
+#             # Method `create` of runbot.build is inherited returning None :(
+#             self.env.cr.execute('SAVEPOINT "build_dummy"')
+#             build.create(
+#                 {'branch_id': branch.id, 'name': 'dummy'})
+#             build_dummy = build.search([
+#                 ('branch_id', '=', branch.id), ('name', '=', 'dummy')],
+#                 limit=1, order='id desc')
+#             repo_id, closest_name, server_match = \
+#                 build_dummy._get_closest_branch_name(branch.repo_id.id)
+#             self.env.cr.execute('ROLLBACK TO SAVEPOINT "build_dummy"')
+#             branch_base = self.search([
+#                 ('repo_id', '=', repo_id), ('name', '=', closest_name)],
+#                 limit=1)
+#             if branch_base.id and branch_base.id != branch.id:
+#                 branch.branch_base_id = branch_base.id
+#                 branch.pr_ok = True
