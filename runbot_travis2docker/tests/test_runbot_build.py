fcb4b1db821d7dfca017b2d9ec64543497905b9c
Moisés López <moylop260@vauxoo.com>
2016-03-23 04:38:15 +0000

    [REF] runbot_travis2docker: Add tests to check if the pull request use image cached

--- runbot_travis2docker/tests/test_runbot_build.py
+++ runbot_travis2docker/tests/test_runbot_build.py
@@ -9,6 +9,7 @@ import subprocess
 import time
 import xmlrpclib
 
+import openerp
 from openerp.tests.common import TransactionCase
 
 
@@ -26,21 +27,54 @@ class TestRunbotJobs(TransactionCase):
             ('is_travis2docker_build', '=', True)], limit=1)
         self.repo_domain = [('repo_id', '=', self.repo.id)]
 
+    def delete_build_path(self, build):
+        subprocess.check_output(['rm', '-rf', build.path()])
+
+    def delete_image_cache(self, build):
+        cmd = ['docker', 'rmi', '-f', build.docker_image_cache]
+        res = -1
+        try:
+            res = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError:
+            pass
+        return res
+
+    def delete_container(self, build):
+        cmd = ['docker', 'rm', '-f', build.get_docker_container()]
+        res = -1
+        try:
+            res = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError:
+            pass
+        return res
+
+    @openerp.tools.mute_logger('openerp.addons.runbot.runbot')
     def wait_change_job(self, current_job, build,
-                        loops=36, timeout=10):
-        for _ in range(loops):
+                        loops=40, timeout=20):
+        _logger.info("Waiting change of current job: %s", current_job)
+        for count in range(loops):
             self.repo.cron()
             if build.job != current_job:
-                break
+                return build.job
             time.sleep(timeout)
-        return build.job
+            if divmod(count + 1, 5)[1] == 0:
+                _logger.info("...")
+        # The build don't changed of job.
+        return False
 
-    def test_jobs(self):
-        'Create build and run all jobs'
+    def test_10_jobs_branch(self):
+        "Create build and run all jobs in branch case (not pull request)"
+        self.run_jobs('fast-travis')
+
+    def test_20_jobs_pr(self):
+        "Create build and run all jobs in pull request"
+        self.run_jobs('pull/1')
+
+    def run_jobs(self, branch):
         self.assertEqual(len(self.repo), 1, "Repo not found")
         self.repo.update()
         branch = self.branch_obj.search(self.repo_domain + [
-            ('name', 'like', 'fast')], limit=1)
+            ('name', 'like', branch)], limit=1)
         self.assertEqual(len(branch), 1, "Branch not found")
         self.build_obj.search([('branch_id', '=', branch.id)]).unlink()
 
@@ -48,22 +82,33 @@ class TestRunbotJobs(TransactionCase):
         build = self.build_obj.search([
             ('branch_id', '=', branch.id)], limit=1)
         self.assertEqual(len(build) == 0, False, "Build not found")
+
+        if build.state == 'done' and build.result == 'skipped':
+            # When the last commit of the repo is too old,
+            # runbot will skip this build then we are forcing it
+            build.force()
+
+        build.checkout()
+        self.delete_build_path(build)
         self.assertEqual(
             build.state, u'pending', "State should be pending")
 
         self.repo.cron()
         self.assertEqual(
             build.state, u'testing', "State should be testing")
-        self.assertEqual(
-            build.job, u'job_10_test_base',
-            "Job should be job_10_test_base")
-        new_current_job = self.wait_change_job(build.job, build)
-        _logger.info(open(os.path.join(build.path(), "logs",
-                                       "job_10_test_base.txt")).read())
+        if not build.is_pull_request:
+            self.assertEqual(
+                build.job, u'job_10_test_base',
+                "Job should be job_10_test_base")
+            new_current_job = self.wait_change_job(build.job, build)
+            _logger.info(
+                open(os.path.join(build.path(), "logs",
+                                  "job_10_test_base.txt")).read())
+        else:
+            new_current_job = u'job_20_test_all'
 
         self.assertEqual(
-            new_current_job, u'job_20_test_all',
-            "Job should be job_20_test_all")
+            new_current_job, u'job_20_test_all')
         new_current_job = self.wait_change_job(new_current_job, build)
         _logger.info(open(
             os.path.join(build.path(), "logs",
@@ -76,7 +121,7 @@ class TestRunbotJobs(TransactionCase):
             build.state, u'running',
             "Job state should be running")
 
-        time.sleep(360)
+        user_ids = self.connection_test(build, 36, 10)
         _logger.info(open(
             os.path.join(build.path(), "logs",
                          "job_30_run.txt")).read())
@@ -84,21 +129,31 @@ class TestRunbotJobs(TransactionCase):
         self.assertEqual(
             build.state, u'running',
             "Job state should be running still")
-        user_ids = self.connection_test(build)
         self.assertEqual(
             len(user_ids) >= 1, True, "Failed connection test")
 
+        self.assertEqual(
+            build.result, u'ok', "Job result should be ok")
+        self.assertTrue(
+            self.exists_container(build), "Container dont't exists")
         build.kill()
         self.assertEqual(
             build.state, u'done', "Job state should be done")
+        self.assertFalse(
+            self.exists_container(build), "Container don't deleted")
+        if not build.is_pull_request:
+            self.assertTrue(
+                self.docker_registry_test(build),
+                "Docker image don't found in registry.",
+            )
+            self.delete_image_cache(build)
 
-        self.assertEqual(
-            build.result, u'ok', "Job result should be ok")
-
-        self.assertTrue(
-            self.docker_registry_test(build),
-            "Docker image don't found in registry.",
-        )
+    def exists_container(self, build):
+        cmd = ['docker', 'ps']
+        containers = subprocess.check_output(cmd)
+        if build.get_docker_container() in containers:
+            return True
+        return False
 
     def docker_registry_test(self, build):
         cmd = [
@@ -108,24 +163,29 @@ class TestRunbotJobs(TransactionCase):
         ]
         tag_list_output = subprocess.check_output(cmd)
         tag_build = build.docker_image_cache.split(':')[-1]
-        if tag_build in tag_list_output:
-            return True
-        else:
-            return False
+        return tag_build in tag_list_output
 
-    def connection_test(self, build):
+    def connection_test(self, build, attempts=1, delay=0):
         username = "admin"
         password = "admin"
         database_name = "openerp_test"
         port = build.port
         host = '127.0.0.1'
-        sock_common = xmlrpclib.ServerProxy(
-            "http://%s:%d/xmlrpc/common" % (host, port))
-        uid = sock_common.login(
-            database_name, username, password)
-        sock = xmlrpclib.ServerProxy(
-            "http://%s:%d/xmlrpc/object" % (host, port))
-        user_ids = sock.execute(
-            database_name, uid, password, 'res.users',
-            'search', [('login', '=', 'admin')])
+        user_ids = []
+        for _ in range(attempts):
+            try:
+                sock_common = xmlrpclib.ServerProxy(
+                    "http://%s:%d/xmlrpc/common" % (host, port))
+                uid = sock_common.login(
+                    database_name, username, password)
+                sock = xmlrpclib.ServerProxy(
+                    "http://%s:%d/xmlrpc/object" % (host, port))
+                user_ids = sock.execute(
+                    database_name, uid, password, 'res.users',
+                    'search', [('login', '=', 'admin')])
+                _logger.info("Trying connect... connected.")
+                return user_ids
+            except BaseException:
+                _logger.info("Trying connect... failed.")
+            time.sleep(delay)
         return user_ids
