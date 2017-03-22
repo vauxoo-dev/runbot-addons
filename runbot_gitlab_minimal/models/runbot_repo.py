f97ff6b6f10e471da0308a694c177e59b4f86782
JesusZapata <jesus@vauxoo.com>
2017-03-22 15:48:48 +0000

    [REF] runbot_gitlab_minimal: Move conditions to better place and document the code

--- runbot_gitlab_minimal/models/runbot_repo.py
+++ runbot_gitlab_minimal/models/runbot_repo.py
@@ -17,6 +17,10 @@ _logger = logging.getLogger(__name__)
 def _get_url(url, base):
     match_object = re.search('([^/]+)/([^/]+)/([^/.]+(.git)?)', base)
     if match_object:
+        """When get is URL_GITHUB/api/v3/User/keys must be convert to
+        URL_GITLAB/User.keys Because the api of gitlab required admin token
+        for get the ssh keys
+        https://docs.gitlab.com/ee/api/users.html#list-ssh-keys"""
         prefix = ('https://%s/api/v3%s'
                   if not url.endswith('/keys') else 'https://%s%s')
         project_name = (match_object.group(2) + '/' + match_object.group(3))
@@ -29,6 +33,9 @@ def _get_url(url, base):
         if url.endswith('/keys'):
             url = url.replace('users/', '').replace('/keys', '')
             url = url + '.keys'
+        if '/pulls/' in url:
+            urls = url.split('/pulls/')
+            url = urls[0] + '/merge_requests?iid=' + urls[1]
     return url
 
 
@@ -93,22 +100,21 @@ class RunbotRepo(models.Model):
             input: URL_GITLAB/projects/... instead of URL_GITHUB/repos/...
             output: res['base']['ref'] = res['gitlab_base_mr']
                     res['head']['ref'] = res['gitlab_head_mr']
+        - Get user public keys
+            input: URL_GITLAB/User.keys... instead of
+                   URL_GITHUB/users/User/keys...
+            output: res['author'] = {'login': data['username']}
+                    res['commiter'] = {'login': data['username']}
         - Report statutes
             input: URL_GITLABL/... instead of URL_GITHUB/statuses/...
             output: N/A
         """
         for repo in self.browse(cr, uid, ids, context=context):
-            is_url_merge = False
-            is_url_keys = False
             if not repo.token:
                 continue
             try:
                 url = _get_url(url, repo.base)
                 if url:
-                    if '/pulls/' in url:
-                        urls = url.split('/pulls/')
-                        url = urls[0] + '/merge_requests?iid=' + urls[1]
-                        is_url_merge = True
                     is_url_keys = url.endswith('.keys')
                     session = _get_session(repo.token)
                     if payload:
@@ -118,7 +124,7 @@ class RunbotRepo(models.Model):
                     response.raise_for_status()
                     json = (response.json() if not is_url_keys
                             else response._content)
-                    if is_url_merge:
+                    if 'merge_requests?iid=' in url:
                         json = json[0]
                         json['head'] = {'ref': json['target_branch']}
                         json['base'] = {'ref': json['source_branch']}
@@ -132,9 +138,10 @@ class RunbotRepo(models.Model):
                                 response = session.get(url)
                                 response.raise_for_status()
                                 data = response.json()
-                                if len(data) == 1:
-                                    data = data[0]
-                                    json[own_key] = {'login': data['username']}
+                                json[own_key] = {
+                                    'login':
+                                    len(data) and data[0]['username'] or {}
+                                }
                     if is_url_keys:
                         json = [{'key': ssh_rsa} for ssh_rsa
                                 in json.split('\n')]
