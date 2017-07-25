d4b793d0be6128046be470c2380557f993b680a4
mapuerta <mauricio@vauxoo.com>
2017-07-25 11:51:26 -0400

    POC add dialog view for add following to builds from frontend

--- runbot_send_email/__openerp__.py
+++ runbot_send_email/__openerp__.py
@@ -18,10 +18,14 @@ email.",
     ],
     "data": [
         "views/runbot_send_email_view.xml",
+        "views/assets.xml",
         "data/runbot_send_email_data.xml",
     ],
     "demo": [
         "demo/ir_mail_server_demo.xml",
+    ],
+    'qweb': [
+        'static/src/xml/runbot_template.xml',
     ],
     "application": False,
     "installable": True,
