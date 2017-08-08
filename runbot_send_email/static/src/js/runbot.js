1e41a8df1cbf0051d608c0bb3eb656e1fb0e9fc8
Mauricio <mauricio@vauxoo.com>
2017-08-07 20:05:26 -0400

    [FIX] runbot_send_email: fixed do not load partners in wizard of the front-end

--- runbot_send_email/static/src/js/runbot.js
+++ runbot_send_email/static/src/js/runbot.js
@@ -10,35 +10,62 @@ var _t = core._t;
 var Qweb = core.qweb;
 Qweb.add_template('/runbot_send_email/static/src/xml/runbot_template.xml');
 
+var _normalize_format_cache = {};
+ 
 $("#lu_add_follower #follower-action-btn" ).on( "click", function(event) {
+    
     var self = this;
     var build_id = $(this).data('runbot-build');
     this.$content = $(Qweb.render('AddFollower'));
+    this.wait = new Dialog(this, {title: _t("<h3>Wait loading data partners</h3>"),
+                                 size: 'medium',
+                                 buttons:[],
+                                 closeButton: false,
+                                 $content: $('<div><i class="fa fa-spinner fa-spin" style="font-size:24px"></i></div>')});
     event.preventDefault;
-    Build.call("select_not_subscribe_partners", [build_id], {}).then(function(data) {
-        for(var i in data){
-            self.$content.find("#sel1").append('<option value='+data[i]['id']+'>'+data[i]['email']+'</option>');
-            }
-    });
-    var options = {
-                title: _t("<h3>Add Followers</h3>"),
-                size: 'medium',
-                buttons: [
-                    { text: _t("Save"), classes: 'btn-primary', close: true, click: function() {
-                      var partners = [];
-                      $("#sel1 option:selected").each(function(ind, element){
-                           partners.push(parseInt($(element).val()));
-                        });
-                      Build.call("add_followers", [[build_id], partners]);
-                     }
-                 },
-                    { text: _t("Close"), close: true }
-                ],
-                 $content: self.$content,
+    this.addfollower = function(self, $content, build_id){
+        var options = {
+        title: _t("<h3>Add Followers</h3>"),
+        size: 'medium',
+        buttons: [
+                { text: _t("Save"), classes: 'btn-primary', close: true, click: function() {
+                var partners = [];
+                $("#sel1 option:selected").each(function(ind, element){
+                    partners.push(parseInt($(element).val()));
+                    var indexof = _normalize_format_cache[build_id].filter(function(elemt, index){
+                    if ($(elemt).val() == $(element).val()){
+                         delete _normalize_format_cache[build_id][index]
+                        }
+                    });
+                });
+                Build.call("add_followers", [[build_id], partners]);
+                }
+                },
+            { text: _t("Close"), close: true }
+            ],
+            $content: $content,
              };
-     this.dialog = new Dialog(this, options).open();
-     this.dialog.$content.find("#sel1").select2({
+            self.dialog = new Dialog(self, options).open();
+            self.dialog.$content.find("#sel1").select2({
             maximumSelectionLength: 3
-         });
-    });
+            });
+        };
+    if (_normalize_format_cache[build_id]) {
+         this.$content.find("#sel1").append(_normalize_format_cache[build_id]);
+         this.addfollower(this, this.$content, build_id);
+        } else {
+        this.wait.$modal.find('.close').remove();
+        this.wait.open();
+        _normalize_format_cache[build_id] = [];
+        Build.call("select_not_subscribe_partners", [build_id], {}).then(function(data) {
+        for(var i in data){
+            _normalize_format_cache[build_id].push('<option value='+data[i]['id']+'>'+data[i]['email']+'</option>');
+            }
+            self.$content.find("#sel1").append(_normalize_format_cache[build_id]);
+            self.wait.destroy();
+        }).done(function(){
+         self.addfollower(this, self.$content, build_id);
+        });
+   };
+  });
 });
