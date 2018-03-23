839853226025dafc230ab2626b991393812bf8f4
Moisés López <moylop260@vauxoo.com>
2018-03-23 17:41:42 +0000

    [REF] runbot_gitlab_minimal: Ran isort

--- runbot_gitlab_minimal/models/runbot_branch.py
+++ runbot_gitlab_minimal/models/runbot_branch.py
@@ -1,7 +1,7 @@
 # Copyright <2017> <Vauxoo info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class RunbotBranch(models.Model):
--- runbot_gitlab_minimal/models/runbot_repo.py
+++ runbot_gitlab_minimal/models/runbot_repo.py
@@ -1,14 +1,14 @@
 # Copyright <2017> <Vauxoo info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import os
 import logging
+import os
 import re
 import urllib.parse
 
 import requests
 
-from odoo import models, fields
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
