d1db2b34668e57be2a46d1e0925f2f8a2e5cbcff
Moisés López <moylop260@vauxoo.com>
2018-04-13 11:56:11 -0500

    small-improvements

--- runbot_send_email/tests/test_runbot_send_email.py
+++ runbot_send_email/tests/test_runbot_send_email.py
@@ -3,13 +3,21 @@
 
 import logging
 import os
+import tempfile
 import threading
+import unittest
 
 from odoo import exceptions
 from odoo.tests.common import TransactionCase
 from odoo.tools import mute_logger
 
 _logger = logging.getLogger(__name__)
+
+SEND_REAL_EMAIL = (
+    os.environ.get('EMAIL_PASSWORD') and os.environ.get('EMAIL_RECIPIENT') and
+    os.environ.get('EMAIL_USER'))
+TEMP_DIR = tempfile.mkdtemp(prefix="runbot_send_email_tmp")
+_logger.info("temporary directory %s", TEMP_DIR)
 
 
 class TestRunbotSendEmail(TransactionCase):
@@ -24,22 +32,22 @@ class TestRunbotSendEmail(TransactionCase):
         super(TestRunbotSendEmail, self).setUp()
         repo_name = 'https://github.com/owner/repo_name.git'
         branch_name = 'refs/heads/branch_name'
-        self.repo_obj = self.env['runbot.repo'].create({
+        self.repo = self.env['runbot.repo'].create({
             'name': repo_name,
             'modules_auto': 'repo',
             'mode': 'poll',
             'nginx': True,
         })
-        self.branch_obj = self.env['runbot.branch'].create({
-            'repo_id': self.repo_obj.id,
+        self.branch = self.env['runbot.branch'].create({
+            'repo_id': self.repo.id,
             'name': branch_name,
             'branch_name': 'branch_name',
             'sticky': False,
             'coverage': False,
         })
         recipient = os.environ.get('EMAIL_RECIPIENT', 'committer@testsend.com')
-        self.build_obj = self.env['runbot.build'].create({
-            'branch_id': self.branch_obj.id,
+        self.build = self.env['runbot.build'].create({
+            'branch_id': self.branch.id,
             'name': 'fcb98eb195fc62fa49873f8f101f1738e38ea7c0',
             'author': 'Test Author',
             'author_email': 'author@testsend.com',
@@ -47,19 +55,18 @@ class TestRunbotSendEmail(TransactionCase):
             'committer_email': recipient,
             'subject': '[TEST] Test message commit',
             'date': '2016-03-08 00:00:00',
+            'state': 'running',
         })
-        self.domain = [('repo_id', '=', self.repo_obj.id)]
-        self.build = self.build_obj.search(self.domain)
-        if os.environ.get('EMAIL_PASSWORD', False) and \
-                os.environ.get('EMAIL_RECIPIENT', False) and \
-                os.environ.get('EMAIL_USER', False):
+        self.template = self.build.get_email_template()
+        self.template.auto_delete = False
+        if SEND_REAL_EMAIL:
             setattr(threading.currentThread(), 'testing', False)
             self.build.pool._init = False
             self.mail_server = self.env.ref(
                 'runbot_send_email.runbot_send_ir_mail_server_demo')
             self.mail_server.write({
-                'smtp_pass': os.environ.get('EMAIL_PASSWORD'),
-                'smtp_user': os.environ.get('EMAIL_USER'),
+                'smtp_pass': os.environ['EMAIL_PASSWORD'],
+                'smtp_user': os.environ['EMAIL_USER'],
                 'sequence': 0,
             })
 
@@ -72,49 +79,36 @@ class TestRunbotSendEmail(TransactionCase):
         setattr(threading.currentThread(), 'testing', True)
         self.build.pool._init = True
 
-    def test_10_test_connection(self):
+    @unittest.skipIf(not SEND_REAL_EMAIL,
+                     "If real values are not used then skip")
+    def test_test_connection(self):
+        """This makes the test smtp conection if EMAIL_PASSWORD env is set
         """
-        This makes the test smtp conection if EMAIL_PASSWORD env is set
-        """
-        if os.environ.get('EMAIL_PASSWORD', False) and \
-                os.environ.get('EMAIL_RECIPIENT', False) and \
-                os.environ.get('EMAIL_USER', False):
-            with self.assertRaisesRegex(exceptions.except_orm,
-                                        'Connection Test Succeeded!'):
-                self.mail_server.test_smtp_connection()
+        with self.assertRaisesRegex(exceptions.except_orm,
+                                    'Connection Test Succeeded!'):
+            self.mail_server.test_smtp_connection()
 
-    def test_20_send_email(self):
-        """
-        This test tries to send a runbot email with password
-        """
+    def test_send_email_result_ok(self):
+        self.build.result = 'ok'
         self.build._github_status()
+        mail = self.get_last_build_mail(self.build)
 
-    def test_30_send_email_result_ok(self):
-        self.build = self.build.search(self.domain)
-        self.build.write({'result': 'ok', 'state': 'done'})
+    def test_send_email_result_ko(self):
+        self.build.result = 'ko'
         self.build._github_status()
+        mail = self.get_last_build_mail(self.build)
 
-    def test_30_send_email_result_ko(self):
-        self.build = self.build.search(self.domain)
-        self.build.write({'result': 'ko'})
+    def test_send_email_result_warn(self):
+        self.build.result = 'warn'
         self.build._github_status()
+        mail = self.get_last_build_mail(self.build)
 
-    def test_30_send_email_result_warn(self):
-        self.build = self.build.search(self.domain)
-        self.build.write({'result': 'warn', 'state': 'done'})
+    def test_send_email_branch_pr(self):
+        self.branch.write({'name': 'refs/pull/1'})
         self.build._github_status()
+        mail = self.get_last_build_mail(self.build)
 
-    def test_40_send_email_state_testing(self):
-        self.build = self.build.search(self.domain)
-        self.build.write({'state': 'testing'})
-        self.build._github_status()
-
-    def test_50_send_email_brach_pr(self):
-        self.branch_obj.write({'name': 'refs/pull/1'})
-        self.build = self.build.search(self.domain)
-        self.build._github_status()
-
-    def test_80_user_follow_unfollow_runbot_build(self):
+    def test_user_follow_unfollow_runbot_build(self):
         """Test for the method user_follow_unfollow for the model runbot.build.
         """
         user = self.env['res.users'].browse(self.env.uid)
@@ -125,30 +119,36 @@ class TestRunbotSendEmail(TransactionCase):
         self.assertFalse(result)
         self.assertFalse(self.build.message_partner_ids)
 
-    def test_81_user_follow_unfollow_runbot_repo(self):
+    def test_user_follow_unfollow_runbot_repo(self):
         """Test for the method user_follow_unfollow for the model runbot.repo.
         """
-        user = self.env['res.users'].browse(self.env.uid)
         result = self.build.repo_id.user_follow_unfollow()
-        followers = self.build.repo_id.message_partner_ids
         self.assertTrue(result)
+        user = self.env['res.users'].browse(self.env.uid)
+        followers = self.build.repo_id.message_partner_ids
         self.assertEquals(user.partner_id, followers[0])
         result = self.build.repo_id.user_follow_unfollow()
         self.assertFalse(result)
         self.assertFalse(self.build.repo_id.message_partner_ids)
 
+    def get_last_build_mail(self, build):
+        mail = self.env['mail.mail'].search([
+            ('res_id', '=', build.id), ('model', '=', build._name),
+        ], limit=1, order='id desc')
+        temp_path = os.path.join(TEMP_DIR, "%s.html" % self._testMethodName)
+        with open(temp_path, "w") as ftmp:
+            ftmp.write(mail.body or '')
+        return mail
+
     @mute_logger('odoo.addons.mail.models.mail_mail')
-    def test_90_send_email(self):
+    def test_send_email(self):
         """Test for the method send_email_admins.
         """
-        self.build.message_subscribe_users(user_ids=[self.env.uid])
-        template = self.build.get_email_template()
-        template.auto_delete = False
-        self.build.send_email()
-        mail = self.env['mail.mail'].search([
-            ('res_id', '=', self.build.id), ('model', '=', self.build._name),
-        ], limit=1, order='id desc')
+        result = self.build.user_follow_unfollow()
+        self.assertTrue(result)
         emails = self.build.message_partner_ids.mapped('email')
-        self.assertTrue(mail)
+        self.assertTrue(emails)
+        self.build.send_email()
+        mail = self.get_last_build_mail(self.build)
         self.assertTrue(mail.body)
-        self.assertEqual(','.join(emails), mail.email_to)
+        self.assertEquals([mail.email_to], emails)
